f2c3df7046392d39af0e5aa7ee2bed54012f54a2
Air <air@instyes-MacBook-Air.local>
2019-04-09 22:14:07 +0300

    2js

--- tasks/TASK-1.js
+++ tasks/TASK-1.js
@@ -1,57 +1,92 @@
-/**
- * 
- * Нужно сделать рендеринг списка заметок (как в прошлый раз), но теперь вместо заверстанных 
- * элементов списка, нужно написать скрипт который будет их добавлять после загрузки страницы. 
- * 
- * В начале скрипта добавь тот обьект, что ниже и отрисуй список заметок из него. 
- * Если у заметки флаг "highligh" установлен в true, нужно визуально (любым способом) выделять ее среди других
- */
+// /**
+//  * 
+//  * Нужно сделать рендеринг списка заметок (как в прошлый раз), но теперь вместо заверстанных 
+//  * элементов списка, нужно написать скрипт который будет их добавлять после загрузки страницы. 
+//  * 
+//  * В начале скрипта добавь тот обьект, что ниже и отрисуй список заметок из него. 
+//  * Если у заметки флаг "highligh" установлен в true, нужно визуально (любым способом) выделять ее среди других
+//  */
 
- const notesData = {
-   items: [
-    {
-      text: "заметка 1",
-      highlight: false,
-    },
-    {
-      text: "заметка 2",
-      highlight: true,
-    },
-    {
-      text: "заметка 3",
-      highlight: false,
-    },
-    {
-      text: "заметка 4",
-      highlight: false,
-    },
-   ],
- }
-document.getElementsByClassName('column');
-allert ();
+//  const notesData = {
+//    items: [
+//     {
+//       text: "заметка 1",
+//       highlight: false,
+//     },
+//     {
+//       text: "заметка 2",
+//       highlight: true,
+//     },
+//     {
+//       text: "заметка 3",
+//       highlight: false,
+//     },
+//     {
+//       text: "заметка 4",
+//       highlight: false,
+//     },
+//    ],
+//  }
 
- const first = document.createElement('div');
+
+ var first = document.createElement('div');
  first.classList.add('first');
+ var textLeft = document.createElement('span');
+ textLeft.classList.add('textLeft');
+ textLeft.innerText = 'В процессе'
+ first.appendChild(textLeft);
+ element.appendChild(first);
+ var element = document.getElementsById('column');
 
- const row = document.createElement('div');
- row.classList.add('row');
+//  const row = document.createElement('div');
+//  row.classList.add('row');
 
- const row = document.createElement('div');
- row.classList.add('row');
+//     const textDiv = document.createElement('div');
+//     textDiv.innerText = input.value;
+ 
+//     const penIcon = document.createElement('div');
+//     penIcon.classList.add('second');
+ 
+//     const fasFaPen = document.createElement('i');
+//     fasFaPen.classList.add('fas');
+//     fasFaPen.classList.add('fa-pen');
 
- const row = document.createElement('div');
- row.classList.add('row');
+//  const row = document.createElement('div');
+//  row.classList.add('row');
 
- const plusText = document.createElement('div');
- plusText.classList.add('plusText');
+//      const textDiv = document.createElement('div');
+//     textDiv.innerText = input.value;
+ 
+//     const penIcon = document.createElement('div');
+//     penIcon.classList.add('second');
+ 
+//     const fasFaPen = document.createElement('i');
+//     fasFaPen.classList.add('fas');
+//     fasFaPen.classList.add('fa-pen');
 
- const fieldText = document.createElement('div');
- fieldText.classList.add('fieldText');
+//  const row = document.createElement('div');
+//  row.classList.add('row');
+ 
+//      const textDiv = document.createElement('div');
+//     textDiv.innerText = input.value;
+ 
+//     const penIcon = document.createElement('div');
+//     penIcon.classList.add('second');
+ 
+//     const fasFaPen = document.createElement('i');
+//     fasFaPen.classList.add('fas');
+//     fasFaPen.classList.add('fa-pen');
 
- const inputText = document.createElement('input');
- inputText.classList.add('.plustext input[type=text]');
+//  const plusText = document.createElement('div');
+//  plusText.classList.add('plusText');
 
- const buttonAdd = document.createElement('button');
- const fasFaPlus = document.createElement('i');
- fasFaPen.classList.add('fas');
- fasFaPen.classList.add('fa-plus');
+//  const fieldText = document.createElement('div');
+//  fieldText.classList.add('fieldText');
+
+//  const inputText = document.createElement('input');
+//  inputText.classList.add('.plustext input[type=text]');
+
+//  const buttonAdd = document.createElement('button');
+//  const fasFaPlus = document.createElement('i');
+//  fasFaPen.classList.add('fas');
+//  fasFaPen.classList.add('fa-plus');
